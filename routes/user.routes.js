a39fa9c3519475d5762ee6e466640127903c7bb1
PriyanshuPatil <pantuugurjar@gmail.com>
2023-12-20 00:27:01 +0530

    added

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,23 +1,108 @@
-const express = require("express");
-const { register, login, getUser, getSingleUser, updateUser, deleteUser } = require("../controller/user.controller");
-const usersRouter = express.Router();
+var jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const { userModel} = require("../model/user.model");
 
-// To login/generate a new token.
-usersRouter.post("/login", login);
+const getUser = async (req, res) => {
+  try {
+    const user_Data = await userModel.find();
+    res.send(user_Data);
+  } catch (err) {
+    res.status(400).send({ msg: err.message });
+  }
+};
 
-// To get user data.
-usersRouter.get("/", getUser);
+const updateUser = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const user_Data = await userModel.findByIdAndUpdate(
+      { _id: id },
+      { ...req.body }
+    );
+    res.send({ msg: "User Details Succesfully Updated" });
+  } catch (err) {
+    res.status(400).send({ msg: err.message });
+  }
+};
 
-// To register a new user.
-usersRouter.post("/register", register);
+const getSingleUser = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const user_Data = await userModel.findById(id);
+    if (user_Data) {
+      res.send(user_Data);
+    } else {
+      res.send({ msg: "Account is Not Present" });
+    }
+  } catch (err) {
+    res.status(400).send({ msg: err.message });
+  }
+};
 
-// To get single user data.
-usersRouter.get("/:id", getSingleUser);
 
-// To update single user data.
-usersRouter.patch("/:id", updateUser);
 
-// To delete single user data.
-usersRouter.delete("/:id", deleteUser);
+const deleteUser = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const user_Data = await userModel.findByIdAndDelete(id);
+    res.send({ msg: "User Account Succesfully Deleted" });
+  } catch (err) {
+    res.status(400).send({ msg: err.message });
+  }
+};
 
-module.exports = { usersRouter};
+const register = async (req, res) => {
+  try {
+    console.log(req.body)
+    let userdata = await userModel.find({ email: req.body.email });
+    if (userdata.length == 0) {
+      bcrypt.hash(req.body.password, 5, async (err, hash) => {
+        if (err) {
+          res.status(400).send({ msg: "Error in password hashing" });
+        } else {
+
+          const newuser = new userModel({ ...req.body, password: hash });
+          await newuser.save();
+          console.log("saved")
+          res.status(201).send({ msg: "User Succesfully Register" });
+        }
+      });
+    } else {
+      res.status(400).send({ msg: "User is allready present" });
+    }
+  } catch (err) {
+    res.status(400).send({ msg: err.message});
+    console.log(err)
+  }
+};
+
+const login = async (req, res) => {
+  console.log(req.body)
+  const { email, password } = req.body.user_data;
+  try {
+    let userdata = await userModel.find({ email });
+    if (userdata.length > 0) {
+       console.log(userdata)
+      bcrypt.compare(password, userdata[0].password, (err, result) => {
+        if (result) {
+          const token = jwt.sign({ userId: userdata[0]._id }, "flex_money");
+          res.send({ msg: "Login Succesfully", token: token , email ,username: userdata[0].username,userId:userdata[0]._id });
+        } else {
+
+          res.status(400).send({ msg: "Password Is Wrong" });
+        }
+      });
+    } else {
+      console.log(email)
+      res.status(404).send({ msg: "Email is not Registered" });
+    }
+  } catch (err) {
+    res.status(400).send({ msg: err.message });
+  }
+};
+module.exports = {
+  getUser,
+  register,
+  login,getSingleUser,
+  updateUser,
+  deleteUser,
+};
